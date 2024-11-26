67801ee05d4828c26a91f9a5d4f4fae37f4faca0
Vorotnikov-Vlad <Vladislav@rothausendevelopment.com>
2024-11-26 17:03:07 +0200

    Yet another fix for game pass.

--- Stalker2PakCfgMergeTool/Program.cs
+++ Stalker2PakCfgMergeTool/Program.cs
@@ -13,11 +13,6 @@ public class Program
     private const string PaksDirectory = "Stalker2/Content/Paks";
     private const string ModsDirectoryName = "~mods";
     private const string ReferencePakName = "pakchunk0-Windows.pak";
-    private const string Win64ExeFilePath = "Stalker2/Binaries/Win64/Stalker2-Win64-Shipping.exe";
-    private const string WinGdkExeFilePath = "Content/Stalker2/Binaries/WinGDK/Stalker2-WinGDK-Editor.exe";
-
-    private static string? _exeFilePath;
-    private static string? _paksDirectory;
 
     public static async Task Main(string[] args)
     {
@@ -37,15 +32,6 @@ public class Program
         }
     }
 
-    private static void ShowInvalidPathMessage()
-    {
-        Console.WriteLine("The game executable file not found in the provided path.");
-        Console.WriteLine("Please provide the path to the game folder as the first argument.\n");
-        Console.WriteLine("Example: Stalker2PakCfgMergeTool.exe \"D:\\Games\\Steam Games\\steamapps\\common\\S.T.A.L.K.E.R. 2 Heart of Chornobyl\"\n");
-        Console.WriteLine("Press any key to exit...");
-        Console.ReadKey();
-    }
-
     private static async Task Execute(string gamePath)
     {
         if (string.IsNullOrEmpty(gamePath) || !Directory.Exists(gamePath))
@@ -54,32 +40,26 @@ public class Program
             return;
         }
 
-        if (File.Exists(Path.Combine(gamePath, Win64ExeFilePath)))
+        var gameRootDirInfo = new DirectoryInfo(gamePath);
+
+        // game pass version has Content folder in the root directory
+        if (gameRootDirInfo.GetDirectories().Select(di => di.Name).Contains("Content"))
         {
-            _exeFilePath = Win64ExeFilePath;
-            _paksDirectory = PaksDirectory;
-        }
-        else if (File.Exists(Path.Combine(gamePath, WinGdkExeFilePath)))
-        {
-            _exeFilePath = WinGdkExeFilePath;
-            _paksDirectory = Path.Combine("Content", PaksDirectory);
-        }
-        else
-        {
-            ShowInvalidPathMessage();
-            return;
+            gamePath = Path.Combine(gamePath, "Content");
         }
 
-        var aesKey = AesKeyGetter.Get(Path.Combine(gamePath, _exeFilePath)) ?? throw new Exception("AES key not found");
+        var exeFilePath = GetExeFilePath(gamePath);
+        var paksDirectory = Path.Combine(gamePath, PaksDirectory);
 
+        var aesKey = AesKeyGetter.Get(Path.Combine(gamePath, exeFilePath)) ?? throw new Exception("AES key not found");
 
         Console.WriteLine($"\nYour AES key: {aesKey}\n");
 
-        var modsPath = Path.Combine(gamePath, _paksDirectory, ModsDirectoryName);
+        var modsPath = Path.Combine(gamePath, paksDirectory, ModsDirectoryName);
 
         using var pakMerger = new PakMerger(
             new Cue4PakProvider(modsPath, aesKey),
-            new Cue4PakProvider(Path.Combine(gamePath, _paksDirectory), aesKey, ReferencePakName));
+            new Cue4PakProvider(Path.Combine(gamePath, paksDirectory), aesKey, ReferencePakName));
 
         var mergedPakFiles = await pakMerger.MergePaksWithConflicts();
 
@@ -109,7 +89,7 @@ public class Program
 
         var currentDate = DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss");
         var mergedPakName = $"{filePrefix}_{Constants.MergedPakBaseName}_{currentDate}_P.pak";
-        var mergedPakPath = Path.Combine(gamePath, _paksDirectory, ModsDirectoryName, mergedPakName);
+        var mergedPakPath = Path.Combine(gamePath, paksDirectory, ModsDirectoryName, mergedPakName);
 
         var pakCreator = new NetPakCreator();
         pakCreator.CreatePak(Constants.MergedPakBaseName, mergedPakPath, mergedPakFiles);
@@ -128,5 +108,59 @@ public class Program
             };
             Process.Start(processStartInfo);
         }
+    }
+
+    private static string GetExeFilePath(string path)
+    {
+        const string win64 = "Win64";
+        const string winGdk = "WinGDK";
+        const string binariesDirectory = "Stalker2/Binaries";
+        const string shipping = "Shipping";
+        const string editor = "Editor";
+
+        path = Path.Combine(path, binariesDirectory);
+
+        string win;
+        var directories = new DirectoryInfo(path).GetDirectories().Select(di => di.Name).ToList();
+
+        if (directories.Contains(win64))
+        {
+            win = win64;
+        }
+        else if (directories.Contains(winGdk))
+        {
+            win = winGdk;
+        }
+        else
+        {
+            throw new Exception($"{path} path does not contain Win64 or WinGDK directory.");
+        }
+
+        path = Path.Combine(path, win);
+        var exeFile = $"Stalker2-{win}";
+
+        var shippingExePath = Path.Combine(path, $"{exeFile}-{shipping}.exe");
+        var editorExePath = Path.Combine(path, $"{exeFile}-{editor}.exe");
+
+        if (File.Exists(shippingExePath))
+        {
+            return Path.Combine(path, shippingExePath);
+        }
+
+        if (File.Exists(editorExePath))
+        {
+            return Path.Combine(path, editorExePath);
+        }
+
+        throw new Exception($"The game executable file not found in the provided path: {path}");
+    }
+
+    private static void ShowInvalidPathMessage()
+    {
+        Console.WriteLine("The game executable file not found in the provided path.");
+        Console.WriteLine("Please provide the path to the game folder as the first argument.\n");
+        Console.WriteLine("Example: Stalker2PakCfgMergeTool.exe \"D:\\Games\\Steam Games\\steamapps\\common\\S.T.A.L.K.E.R. 2 Heart of Chornobyl\"\n");
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
     }
 }
